88850d99e0909900a4f465dc25ea569f690deca7
D4nielJ <d4niel.djm@gmail.com>
2021-06-22 09:53:23 -0500

    Add query selectors for popup

--- src/main.js
+++ src/main.js
@@ -25,12 +25,12 @@ const projectFeature = document.querySelector('.project-feature');
 const projectBack = document.querySelector('.project-background');
 const projectYear = document.querySelector('.project-year');
 const projectImage = document.querySelector('.project-image');
-// const projectDescription = document.querySelector('.project-description');
-// const projectTool1 = document.querySelector('.project-tool1');
-// const projectTool2 = document.querySelector('.project-tool2');
-// const projectTool3 = document.querySelector('.project-tool3');
-// const projectLinkLive = document.querySelector('.project-linkLive');
-// const projectLinkSource = document.querySelector('.project-linkSource');
+const projectDescription = document.querySelector('.project-description');
+const projectTool1 = document.querySelector('.project-tool1');
+const projectTool2 = document.querySelector('.project-tool2');
+const projectTool3 = document.querySelector('.project-tool3');
+const projectLinkLive = document.querySelector('.project-linkLive');
+const projectLinkSource = document.querySelector('.project-linkSource');
 
 // const openModal = (btn) => {
 //   let button = btn.path[0];

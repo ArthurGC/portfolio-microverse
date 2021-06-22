6d950b3e86c2fd52fe0efaeaa49b0a3a9ed2461f
arthurGC <aguzmanc@unsa.edu.pe>
2021-06-22 11:07:03 -0500

    Fix indentation

--- src/menu.js
+++ src/menu.js
@@ -2,9 +2,7 @@ export const menuIconMobile = document.querySelector('.navbar__hamb');
 const menu = document.querySelector('.navbar__menu');
 export const menuLinks = document.querySelectorAll('.navbar__menu__link');
 
-const isActiveClassExisted = (element) => {
-  return element.classList.contains('active');
-}
+const isActiveClassExisted = (element) => element.classList.contains('active');
 
 const refreshIcon = () => {
   if (isActiveClassExisted(menu)) {
@@ -12,14 +10,13 @@ const refreshIcon = () => {
   } else {
     menuIconMobile.setAttribute('src', 'images/hamburguer.png');
   }
-}
+};
 export const openMenu = () => {
   menu.classList.toggle('active');
   refreshIcon();
-}
+};
 
 export const closeMenu = () => {
   menu.classList.remove('active');
   refreshIcon();
-}
-
+};

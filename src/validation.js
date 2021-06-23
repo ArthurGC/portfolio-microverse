0a8b1cc37bfbbe511bf6d3e7dba871976d4f0d89
D4nielJ <d4niel.djm@gmail.com>
2021-06-23 10:56:19 -0500

    Clean validation.js file

--- src/validation.js
+++ src/validation.js
@@ -2,16 +2,6 @@ export const email = document.querySelector('.contact__email');
 export const form = document.querySelector('#form');
 export const error = document.querySelector('.error');
 
-// export const isEmailRight = (string) => {
-//     string.forEach(letter => {
-//         if (letter.match(/^[A-Z]*$/)) {
-//             return false;
-//         }
-
-//     })
-//     return true;
-// }
-
 export function isItUppercase(value) {
     if (value.match(/^[a-z@.]*$/)) {
         return true;

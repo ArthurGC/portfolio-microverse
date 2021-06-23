ff9c3a7efcf1436d425710973d26a8c1cc1d02f1
arthurGC <aguzmanc@unsa.edu.pe>
2021-06-23 11:40:57 -0500

    Fix validation

--- src/validation.js
+++ src/validation.js
@@ -3,7 +3,7 @@ export const form = document.querySelector('#form');
 export const error = document.querySelector('.error');
 
 export function isItUppercase(value) {
-  if (value.match(/^[a-z@.]*$/)) {
+  if (value.match(/^[a-z@.0-9-_]*$/)) {
     return true;
   }
   return false;
